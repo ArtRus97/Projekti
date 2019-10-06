7f38cf82750bf5e4bafbbbb6a3a106bd88500426
Arttu Rusanen <t6ruar00@students.oamk.fi>
2019-10-06 22:52:52 +0300

    korjauksia

--- tuotteet/Mordhau_ostoskori.php
+++ tuotteet/Mordhau_ostoskori.php
@@ -1,10 +1,10 @@
 <div id="ostoskori-esittely">
-  <a href="mordhau_sivu.php"><h3 class="nimi">Mordhau</h3></a> <br>
-  <p><a href="mordhau_sivu.php"><img src="kuvat/mordhau.jpg" alt=""></a>
+  <a href="mordhau_sivu.php"><h3 class="nimi">Mordhau</h3> <br>
+  <p><img src="kuvat/mordhau.jpg" alt="">
     DEVELOPER: Triternion.
     PUBLISHER: Triternion.
     MORDHAU is a multiplayer medieval slasher. Create your mercenary and fight in brutal battles where you will experience fast paced combat, castle sieges, cavalry charges, and more.
-  </p>
+  </p></a>
   <h3>Määrä: <input type="number" name="" value="1" min="1">
       25€</h3>
 </div>
